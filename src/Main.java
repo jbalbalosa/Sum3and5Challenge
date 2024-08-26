604df980f010556258d82e0eb11ddb4db94788df
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2024-08-26 15:08:10 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Sum 3 and 5 Challenge!");
+
+        int sum = 0;
+        int count = 0;
+        for(int i = 30; i <70; i++){
+            if(i % 3 == 0 && i % 5 == 0){
+                sum += i;
+                count++;
+                System.out.println( i + " number can be divided by 3 or 5");
+            }
+            if (count == 3){
+                System.out.println("Sum of all numbers :" + sum);
+                break;
+            }
+        }
     }
 }
